04b2079c75d1695bf0df9a724413e3a0799a0f60
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2024-09-13 21:18:45 -0400

    many files now compile with coq 8.19 using lem-lib in https://github.com/aa755/lemcoqlib

--- lem/helper.v
+++ lem/helper.v
@@ -1,9 +1,10 @@
 Require Import Bvector.
 Require Export Bvector.
 
-Require Import Zdigits.
-Require Export Zdigits.
+Require Import Evm.Zdigits.
+Require Export Evm.Zdigits.
 
+(*
 Module ListNotations.
 Notation "[ ]" := nil (format "[ ]") : list_scope.
 Notation "[ x ]" := (cons x nil) : list_scope.
@@ -12,9 +13,11 @@ Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..) (compat "8.4") : list_s
 End ListNotations.
 
 Import ListNotations.
+ *)
+Require Import Coq.Lists.List.
 Export ListNotations.
 
 Open Scope list_scope.
 
-
-Definition word256_default := Z_to_binary 256 0.
+Require Import ZArith.
+Definition word256_default := Z_to_binary 256 0%Z.
